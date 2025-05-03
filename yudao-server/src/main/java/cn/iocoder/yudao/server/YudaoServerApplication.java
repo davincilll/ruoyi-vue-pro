6db9ef38a303568c05bd8204c7e0059432959b83
dfcold <3135287831@qq>
2025-05-03 15:30:46 +0800

    refactor: 更新数据库和 Redis配置
    - 修改数据库 URL 和密码
    - 更新 Redis 主机和密码
    - 注释掉从库配置
    - 添加待办事项注释

--- yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -12,10 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author 芋道源码
  */
-@SuppressWarnings("SpringComponentScan") // 忽略 IDEA 无法识别 ${yudao.info.base-package}
 @SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.server", "${yudao.info.base-package}.module"})
 public class YudaoServerApplication {
-
+    //Todo 每个模块增加一个MenuProvider，用于生成菜单（这里可以定义多级菜单）
+    // 每个领域增加一个FormProvider，用于生成create和update表单
+    // 每个领域增加一个TableProvider，用于生成表格
+    // 目前现有的权限功能需要拓展 现有的是菜单权限，数据权限，需要拓展操作权限（增删改查）
+    // 然后用一个Builder统一生成应有的菜单，表单，表格
+    // 这里不常修改可以考虑加一层缓存
     public static void main(String[] args) {
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
